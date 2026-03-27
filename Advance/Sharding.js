a8d706faacd8aaf5692fbc5512f3f409096139b6
Nawin chander <nawinnapzy289@gmail.com>
2026-03-27 17:29:32 +0530

    add file and code

--- Advance/Sharding.js
+++ Advance/Sharding.js
@@ -5,5 +5,18 @@
 // Twitter
 
 
+function getShard(userId) {
+  return userId % 3; // simple sharding
+}
+
+
+const dbShards = [db1, db2, db3];
+
+async function getUser(userId) {
+  const shard = getShard(userId);
+  return dbShards[shard].findUser(userId);
+}
+
+
 
 
