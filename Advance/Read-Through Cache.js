77131e37c0fec03d21a7bab696509fd0a6f81610
Nawin chander <nawinnapzy289@gmail.com>
2026-03-27 17:24:23 +0530

    add the code

--- Advance/Read-Through Cache.js
+++ Advance/Read-Through Cache.js
@@ -6,6 +6,22 @@
 // IDEA :  Check cache first
 // If miss → fetch from DB → update cache
 
+// cacheService.js
+async function getUser(userId, cache, db) {
+  const cached = await cache.get(userId);
+
+  if (cached) {
+    return JSON.parse(cached); // 🚀 fast path
+  }
+
+  const user = await db.findUser(userId);
+
+  await cache.set(userId, JSON.stringify(user), 'EX', 60); // TTL 60s
+
+  return user;
+}
+
+
 
 
 
---
+++ Advance/Write-Through Cache.js
@@ -0,0 +1,11 @@
+// Write-Through Cache (Consistency Focus)
+
+// Used when consistency matters (payments, profiles)
+
+// Write to DB AND cache together
+
+
+
+
+
+
