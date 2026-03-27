9bbd1bfb17f64ec2709f37fc84ecad041ce30dc7
Nawin chander <nawinnapzy289@gmail.com>
2026-03-27 17:27:34 +0530

    add the code

---
+++ Advance/Pagination + DB Optimization.js
@@ -0,0 +1,16 @@
+// Pagination + DB Optimization (Scaling Reads)
+
+// Fetching millions of rows → slow
+
+
+// Bad (OFFSET pagination) - SELECT * FROM users LIMIT 10 OFFSET 100000;
+
+// Good (Cursor-based pagination) - SELECT * FROM users WHERE id > 100000 LIMIT 10;
+
+
+
+
+
+
+
+
